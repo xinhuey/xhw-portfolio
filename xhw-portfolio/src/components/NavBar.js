0fb4495219efe9d8d789cb40c6a2d7113d767944
xinhuey <xinhuey.wong@gmail.com>
2024-06-22 22:22:24 -0400

    added Banner animations

--- xhw-portfolio/src/components/NavBar.js
+++ xhw-portfolio/src/components/NavBar.js
@@ -31,14 +31,14 @@ export const NavBar = () => {
     return(
     <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
     <Container>
-        <Navbar.Brand href="#home">React-Bootstrap
-            <img src={logo} alr = "Logo"/>
+        <Navbar.Brand href="/">
+            <img src={logo} alt = "Logo"/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <Nav.Link href="#home" className = {activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick = {() => onUpdateActiveLink('home')}>Home</Nav.Link>
             <Nav.Link href="#skills" className = {activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick = {() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
             <Nav.Link href="#projects" className = {activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick = {() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
