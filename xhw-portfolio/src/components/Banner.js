819cd6086b640867f59fdb01d1c06257f7269403
xinhuey <xinhuey.wong@gmail.com>
2024-06-26 21:03:25 -0400

    added projects section

--- xhw-portfolio/src/components/Banner.js
+++ xhw-portfolio/src/components/Banner.js
@@ -47,7 +47,7 @@ export const Banner = () => {
                 <Row className = "align-items-center">
                     <Col xs={12} md={6} xl={7}>
                     <span className="tagline"> Welcome to my Portfolio!</span>
-                    <h1>{`Hi I'm Xin Huey Wong, a `}<span className="wrap">{text}</span></h1>
+                    <h1>{`Hi, I'm Xin Huey Wong, a `}<span className="wrap">{text}</span></h1>
                     <p>Bla</p>
                     <button onClick={()=>console.log('Connect')}>Let's Connect!<ArrowRightCircle size = {25}/></button>
                     </Col>
