cf9f89d4882df6e451d2abe4174abc51e0ddfe82
xinhuey <xinhuey.wong@gmail.com>
2024-07-30 21:08:02 -0400

    added animation effects

--- xhw-portfolio/src/components/Banner.js
+++ xhw-portfolio/src/components/Banner.js
@@ -2,6 +2,8 @@ import {Container, Row, Col} from  "react-bootstrap";
 import {ArrowRightCircle} from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
 import {useState, useEffect} from 'react';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
 export const Banner = () => {
     const toRotate = ['Software Engineer', 'Computer Science student @ UWaterloo'];
@@ -46,10 +48,15 @@ export const Banner = () => {
             <Container>
                 <Row className = "align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                    <span className="tagline"> Welcome to my Portfolio!</span>
-                    <h1>{`Hi, I'm Xin Huey Wong, a `}<span className="wrap">{text}</span></h1>
-                    <p>Bla</p>
-                    <button onClick={()=>console.log('Connect')}>Let's Connect!<ArrowRightCircle size = {25}/></button>
+                    <TrackVisibility>
+                    {({ isVisible }) =>
+                        <div className ={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                        <span className="tagline"> Welcome to my Portfolio!</span>
+                        <h1>{`Hi, I'm Xin Huey Wong, a `}<span className="wrap">{text}</span></h1>
+                        <p>Bla</p>
+                        <button onClick={()=>console.log('Connect')}>Let's Connect!<ArrowRightCircle size = {25}/></button>
+                    </div>}   
+                    </TrackVisibility>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImg} alt= "Header Img"/>
--- xhw-portfolio/src/components/Projects.js
+++ xhw-portfolio/src/components/Projects.js
@@ -1,10 +1,11 @@
 import {Container, Row, Col, Tab, Nav} from "react-bootstrap";
-
 import {ProjectCard} from './ProjectCard';
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import 'animate.css';
+import TrackVisibility from "react-on-screen";
 
 export const Projects =() => {
     const projects = [
@@ -50,8 +51,13 @@ export const Projects =() => {
             <Container>
                 <Row>
                     <Col>
+                    <TrackVisibility>
+                    {({ isVisible }) =>
+                    <div className ={isVisible ? "animate__animated animate__slideInUp" : ""}>
                         <h2>Projects</h2>
                         <p>These are the projects I have built</p>
+                    </div>}
+                    </TrackVisibility>
                         <Tab.Container id="projects-tabs" defaultActiveKey ="first">
                         <Nav variant="pills" className ="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                           <Nav.Item>
