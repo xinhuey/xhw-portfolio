6dc45e8e18e7460cec8ba98724bc990b88316b46
xinhuey <xinhuey.wong@gmail.com>
2024-07-31 18:53:51 -0400

    linked let's connect to linkedIn

--- xhw-portfolio/src/components/NavBar.js
+++ xhw-portfolio/src/components/NavBar.js
@@ -6,11 +6,15 @@ import navIcon3 from '../assets/img/nav-icon3.svg';
 import logo from '../assets/img/logo.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
+
+    const handleConnect = () =>{
+      window.location.href = "https://www.linkedin.com/in/xhwong/";
+    };
 
     //useEffect triggered when user scrolls
     useEffect(() =>{
@@ -52,7 +56,7 @@ export const NavBar = () => {
                 <a href="https://github.com/xinhuey"><img src={navIcon2} alt=""/></a>
                 <a href="https://www.instagram.com/xinhuey_/"><img src={navIcon3} alt=""/></a>
             </div>
-            <button className = "vvd" onClick ={()=> console.log('Connect')}>
+            <button className = "vvd" onClick ={handleConnect}>
                 <span>Let's Connect</span>
                 <FontAwesomeIcon icon={faArrowRight} style={{ marginLeft: '8px' }} />
             </button>
