7db3897edcaad55e0e4072679448b6821a487b84
xinhuey <xinhuey.wong@gmail.com>
2024-08-19 16:45:29 -0400

    added projects

--- xhw-portfolio/src/components/Projects.js
+++ xhw-portfolio/src/components/Projects.js
@@ -4,45 +4,46 @@ import colorSharp2 from "../assets/img/color-sharp2.png";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import tiles from "../assets/img/tiles.svg";
 import 'animate.css';
 import TrackVisibility from "react-on-screen";
 
 export const Projects =() => {
     const projects = [
         {
-            title: "Business Startup",
-            description: "Design & Development",
+            title: "LinguaStream",
+            description: "Designed and developed the front end of LinguaStream, a web application capable of translating YouTube video audio into any language of choice, using React and Chakra UI. ",
             imgUrl: projImg1 
             
         },
         {
-            title: "Business Startup",
-            description: "Design & Development",
+            title: "CC3K Game",
+            description: "Developed a multi-level game in C++ using different design patterns (Decorator, Factory, MVC).",
             imgUrl: projImg2 
             
         },
         {
-            title: "Business Startup",
-            description: "Design & Development",
+            title: "MIPS Compiler",
+            description: "Developed a compiler in C++ to convert a C-like programming language into MIPS assembly language.",
             imgUrl: projImg3
             
         },
         {
-            title: "Business Startup",
-            description: "Design & Development",
+            title: "ASL Hand Signal Translator",
+            description: "Built a device that recognises sequences of hand signals and constructs English sentences using a machine learning model that was trained to 95% accuracy.",
             imgUrl: projImg1 
             
         },
         {
-            title: "Business Startup",
-            description: "Design & Development",
+            title: "Virtual Bookstore",
+            description: "Developed a full-stack online book store application; using React to develop the frontend interface, leveraging MongoDB for database management and utilized Express.js and Node.js for backend functionalities.",
             imgUrl: projImg2 
             
         },
         {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg3 
+            title: "Tile-Ordering System",
+            description: "Designed and developed a system to capture and process customer orders for tiles in Python.",
+            imgUrl: projImg3
             
         },
     ]
